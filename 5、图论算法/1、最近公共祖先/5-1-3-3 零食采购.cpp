7610d8068ffd95446d406e736f3ec79b01b4dd75
WhereIsHeroFrom <menjitianya2007@163.com>
2026-04-21 13:39:34 +0800

    修改一个勘误

--- 5、图论算法/1、最近公共祖先/5-1-3-3 零食采购.cpp
+++ 5、图论算法/1、最近公共祖先/5-1-3-3 零食采购.cpp
@@ -10,12 +10,14 @@ using namespace std;
 int f[maxn][maxd]; // f[i][j] 代表i号结点的第2^j个祖先
 int dep[maxn];     // dep[i] 代表i在这棵树上的深度
 vector<int> child[maxn];
+int w[maxn], fw[maxn][21];  // fw[i][j]代表从根结点到i结点，有多少个种类为j的零食
 
 void LCA_Init(int n) {
     for(int i = 1; i <= n; ++i) {
         child[i].clear();
     }
     memset(f, dummyroot, sizeof(f));
+    memset(fw, 0, sizeof(fw));
 }
 
 void LCA_AddEdge(int u, int v) {
@@ -26,6 +28,7 @@ void LCA_AddEdge(int u, int v) {
 void LCA_PreProcess(int root) {
     queue<int> q;
     f[root][0] = dummyroot;
+    fw[root][ w[root] ] = 1;
     dep[root] = 0;
     q.push(root);
     while( !q.empty() ) {
@@ -41,6 +44,10 @@ void LCA_PreProcess(int root) {
                 continue;
             }
             f[v][0] = u;
+            // 树上前缀和
+            for(int j = 1; j <= 20; ++j) {
+                fw[v][j] = fw[u][j] + (w[v] == j ? 1 : 0);
+            }
             dep[v] = dep[u] + 1;
             q.push(v);
         }
@@ -71,25 +78,14 @@ int LCA_Get(int u, int v) {
     return f[u][0];
 }
 
-
-/*
-           1
-          /
-         /
-       ....
-        /
-       z
-      / \
-    .......
-     /   \
-     x   y
-*/
-
 int main() {
     int n;
     int q;
     cin >> n >> q;
     LCA_Init(n);
+    for(int i = 1; i <= n; ++i) {
+        cin >> w[i];
+    }
     for(int i = 0; i < n-1; ++i) {
         int x, y;
         cin >> x >> y;
@@ -99,9 +95,14 @@ int main() {
     while(q--) {
         int x, y;
         cin >> x >> y;
-        int z = LCA_Get(x, y);
-        // 容斥原理
-        cout << (dep[x] - dep[z]) + (dep[y] - dep[z]) << endl;
+        int u = LCA_Get(x, y);
+        int ans = 0;
+        // 枚举每一种零食，在 x->u 以及 y->u 的路径和上是否出现过
+        for(int j = 1; j <= 20; ++j) {
+            int val = (fw[x][j] - fw[u][j]) + (fw[y][j] - fw[u][j]) + (w[u] == j ? 1 : 0);
+            ans += (val > 0 ? 1 : 0);
+        } 
+        cout << ans << endl;
     }
     return 0;
 }
